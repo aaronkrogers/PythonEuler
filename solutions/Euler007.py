dc1a97ec9a72dbfa4e70487e8c4a7637b294b495
kyrogon <kyrogon@gmail.com>
2017-04-16 21:42:45 -0600

    Euler007

---
+++ solutions/Euler007.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+---10001st prime---
+
+By listing the first six prime numbers:
+2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+"""
+
+from pyprimes import nth_prime
+
+
+def main():
+    return nth_prime(10001)
+
+
+if __name__ == "__main__":
+    print(main())
