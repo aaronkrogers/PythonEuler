5e2ebedd40b8f6c7539f83be9443d14b1be0984e
kyrogon <kyrogon@gmail.com>
2017-06-02 10:02:26 -0600

    Submitted Euler #9

--- solutions/Euler009.py
+++ solutions/Euler009.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from itertools import combinations
+from math import sqrt
 
 def euler009():
     """
@@ -9,7 +11,10 @@ def euler009():
     for which a + b + c = 1000.
     find the product abc
     """
-    pass
+    for a, b in combinations(range(1, 1000//2), 2):
+        c = sqrt(a ** 2 + b ** 2)
+        if (c%1 == 0) and (a+b+c == 1000):
+            return int(a*b*c)
 
 if __name__ == "__main__":
     print(euler009())
