db262b4de10cfd2c93fb5cf1b9e793d8b0c3e95c
kyrogon <kyrogon@gmail.com>
2017-06-02 09:50:24 -0600

    Started Euler009

---
+++ solutions/Euler009.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def euler009():
+    """
+    --- Special Pythagorean Triplet ---
+    
+    There exists exactly one Pythagorean Triplet
+    for which a + b + c = 1000.
+    find the product abc
+    """
+    pass
+
+if __name__ == "__main__":
+    print(euler009())
