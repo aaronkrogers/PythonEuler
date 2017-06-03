41dff861e8499aa6df834e7fd83e4b145b23cb4f
kyrogon <kyrogon@gmail.com>
2017-06-02 19:21:37 -0600

    Submitted #11

---
+++ solutions/Euler011.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+from os.path import abspath, dirname, pardir
+from itertools import product
+import numpy as np
+
+
+def euler011():
+    """
+    --- Largest product in a grid ---
+    What is the greatest product of four adjacent numbers
+    in the same direction in the 20x20 grid?
+    *Grid given in resources/Euler011.txt*
+    """
+
+    # Set project directory
+    pd = abspath(dirname(__file__) + '/' + pardir)
+    result = 0
+
+    with open(f'{pd}/resources/Euler011.txt', 'r') as grid_file:
+        grid = []
+        for line in grid_file:
+            grid.append([int(item) for item in line.split()])
+
+    grid = np.array(grid)
+    grid_size = len(grid)
+
+    for x, y in product(range(grid_size), range(grid_size)):
+        round_results = []
+        if x < grid_size-3:
+            # Test for down straight
+            round_results.append(grid[x:x+4, y])
+
+        if y < grid_size - 3:
+            # Test for right straight
+            round_results.append(grid[x, y:y + 4])
+
+        if y > 3 and x < grid_size-3:
+            # Test for left-down diagonal
+            nx = range(x, x+4)
+            ny = range(y, y-4, -1)
+            round_results.append(grid[nx, ny])
+
+        if y < grid_size - 3 and x < grid_size-3:
+            # Test for right-down diagonal
+            nx = range(x, x+4)
+            ny = range(y, y+4)
+            round_results.append(grid[nx, ny])
+
+        for r in round_results:
+            if mult(r) > result:
+                result = mult(r)
+
+    return result
+
+
+def mult(iterable):
+    result = 1
+    for item in iterable:
+        result *= item
+    return result
+
+
+if __name__ == "__main__":
+    print(euler011())
