ff06b0b82255c7b7a8ce46af69c6f0047167eb5f
kyrogon <kyrogon@gmail.com>
2017-06-09 15:23:24 -0600

    Added problem description for Euler #13

--- euler/Euler013.py
+++ euler/Euler013.py
@@ -4,6 +4,11 @@ from definitions import RESOURCE_DIR
 
 
 def euler013():
+    """
+    --- Large sum ---
+    Work out the first ten digits of the sum of the
+    one hundred 50-digit numbers located in resources/Euler013.txt
+    """
     result = 0
     with open(f'{RESOURCE_DIR}/Euler013.txt', 'r') as input:
         for line in input:
