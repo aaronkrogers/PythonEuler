25581fc98a186f4ebd7e5ad4f9c16c35479ee024
kyrogon <kyrogon@gmail.com>
2017-06-14 20:37:12 -0600

    Submitted Solution for #19

--- euler/Euler018.py
+++ euler/Euler018.py
@@ -11,6 +11,20 @@ def shrink(triangle):
 
 
 def euler018(filename):
+    """
+    --- Maximum path sum ---
+    By starting at the top of the triangle below and moving to adjacent numbers on the row below,
+    the maximum total from top to bottom is 23.
+
+    3
+    7 4
+    2 4 6
+    8 5 9 3
+    
+    That is, 3 + 7 + 4 + 9 = 23.
+
+    Find the maximum total from top to bottom of the triangle
+    """
     triangle = []
     with open(f'{RESOURCE_DIR}/{filename}', 'r') as triangle_file:
         for line in triangle_file.read().splitlines():
--- euler/Euler019.py
+++ euler/Euler019.py
@@ -2,6 +2,11 @@
 
 
 def euler019():
+    """
+    --- Counting Sundays ---
+    How many Sundays fell on the first of the month during the
+    twentieth century (1 Jan 1901 to 31 Dec 2000)?
+    """
     return int((12 * 100) / 7)
 
 if __name__ == '__main__':
