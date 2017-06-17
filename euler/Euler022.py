29f26436a2e57ecf07f045ba844bc71c07261475
kyrogon <kyrogon@gmail.com>
2017-06-16 22:30:52 -0600

    Submitted solution for #22

---
+++ euler/Euler022.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from definitions import RESOURCE_DIR
+from string import ascii_uppercase
+
+
+def alpha_value(word):
+    result = 0
+    for letter in word:
+        result += ascii_uppercase.index(letter) + 1
+    return result
+
+
+def euler022():
+    with open(f"{RESOURCE_DIR}/Euler022.txt", 'r') as names_file:
+        names = [name.strip('"') for name in names_file.read().split(',')]
+        names.sort()
+
+    result = 0
+
+    for name in names:
+        result += alpha_value(name) * (names.index(name) + 1)
+
+    return result
+
+
+
+if __name__ == "__main__":
+    print(euler022())
