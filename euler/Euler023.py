38adf8aad60ef6731a17f2d4afeb9ef44dbb0162
kyrogon <kyrogon@gmail.com>
2017-06-18 15:05:02 -0600

    Submitted Solution for #23

---
+++ euler/Euler023.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from itertools import combinations_with_replacement
+from Euler021 import factor_sum
+
+
+def is_abundant(n):
+    result = False
+    if factor_sum(n) > n:
+        result = True
+    return result
+
+
+def euler021():
+    """
+    --- Non-abundant sums ---
+    Find the sum of all the positive integers which cannot be written as
+    the sum of two abundant numbers
+    """
+    result  = 0
+    limit = 28123 + 1
+    abundants = (x for x in range(1, limit) if is_abundant(x))
+    abundsums = {sum(x) for x in combinations_with_replacement(abundants, 2)}
+
+    for i in range(1, limit):
+        if i not in abundsums:
+            result += i
+
+    return result
+
+if __name__ == "__main__":
+    print(euler021())
