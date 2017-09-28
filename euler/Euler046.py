ef513706d329e7ed6d0bab11989a8da46aa05c9e
kyrogon <kyrogon@gmail.com>
2017-09-27 20:11:15 -0600

    Submitted solution for #46

---
+++ euler/Euler046.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from itertools import count
+from pyprimes import isprime
+
+
+def euler046() -> int:
+    """Goldbach's other conjecture
+    It was proposed by Christian Goldbach that every odd composite number
+    can be written as the sum of a prime and twice a square.
+
+    It turns out that the conjecture was false.
+
+    What is the smallest odd composite that cannot be written as the sum
+    of a prime and twice a square?
+    """
+    # generator of non-even composite numbers
+    composites = (i for i in count(3, step=2) if not isprime(i))
+    for C in composites:
+        x = 1
+        while (2 * x**2) < C:
+            P = C - (2 * x**2)
+            x += 1
+            if isprime(P):
+                break
+        else:
+            return C
+
+
+if __name__ == "__main__":
+    print(euler046())
