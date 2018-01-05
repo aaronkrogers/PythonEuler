d9944d3aa51941f4e3dca5f79d5576244b9c93db
kyrogon <kyrogon@gmail.com>
2018-01-05 10:22:18 -0700

    Sped up result for Euler #92 Speed up from about 29 seconds to about 4 seconds to return the result

--- euler/Euler092.py
+++ euler/Euler092.py
@@ -1,39 +1,20 @@
 #!/usr/bin/env python3
 
+from itertools import combinations_with_replacement as cwr
+from itertools import permutations
 
-class Chain:
-    def __init__(self):
-        self.known = {1: 1, 89: 89}
 
-    def update(self, values, chain_end):
-        for val in values:
-            self.known[val] = chain_end
-
-    @staticmethod
-    def square_digit_sum(num):
-        result = 0
-        for n in str(num):
-            result += int(n) ** 2
-        return result
-
-    def square_digit_chain(self, num):
-        yield num
-        while num not in (1, 89):
-            num = self.square_digit_sum(num)
-            yield num
-
-    def chain_end(self, num):
-        tmp_chain = set()
-        for val in self.square_digit_chain(num):
-            if val in self.known:
-                result = self.known[val]
-                self.update(tmp_chain, result)
-                return result
-            else:
-                tmp_chain.add(val)
-
-    def __call__(self, num):
-        return self.chain_end(num)
+def square_digit_end(array) -> int:
+    """Return the end result of a square digit chain of a given number
+    :param array: An array of digits in the number to be tested
+    :return: The end of the square digit chain
+    """
+    link = sum(d**2 for d in array)
+    while link not in (0, 1, 89):
+        array = list(str(link))
+        array = [int(d) for d in array]
+        link = sum(d**2 for d in array)
+    return link
 
 
 def euler092() -> int:
@@ -54,11 +35,12 @@ def euler092() -> int:
 
     How many starting numbers below ten million will arrive at 89?
     """
-    chain = Chain()
     result = 0
-    for number in range(1, 10_000_000):
-        if chain(number) == 89:
-            result += 1
+    # Iterate through all combinations of the numbers 0-9 below 10 million
+    for number in cwr(range(10), 7):
+        if square_digit_end(number) == 89:
+            # Add the count of all permutations of the successful digits
+            result += len(set(permutations(number)))
     return result
 
 
