f201239b6b30c4445a3648d120dea2bd753c0516
kyrogon <kyrogon@gmail.com>
2018-01-14 22:25:44 -0700

    Added description for Euler #47

--- euler/Euler047.py
+++ euler/Euler047.py
@@ -4,7 +4,24 @@ import pyprimes
 from itertools import count
 
 
-def euler046():
+def euler047():
+    """Distinct Prime Factors
+    The first two consecutive numbers to have
+    two distinct prime factors are:
+
+        14 = 2 × 7
+        15 = 3 × 5
+
+    The first three consecutive numbers to have
+    three distinct prime factors are:
+
+        644 = 2² × 7 × 23
+        645 = 3 × 5 × 43
+        646 = 2 × 17 × 19.
+
+    Find the first four consecutive integers to have four distinct
+    prime factors each. What is the first of these numbers?
+    """
     for C in count(644):
         for i in range(4):
             if len(list(pyprimes.factorise(C+i))) != 4:
@@ -14,4 +31,4 @@ def euler046():
 
 
 if __name__ == "__main__":
-    print(euler046())
+    print(euler047())
