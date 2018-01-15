6c18b3c8be2e7dda36713e9643f011d5dc440c94
kyrogon <kyrogon@gmail.com>
2018-01-14 22:20:01 -0700

    improved solution to Euler #47

--- euler/Euler047.py
+++ euler/Euler047.py
@@ -5,7 +5,7 @@ from itertools import count
 
 
 def euler046():
-    for C in count(1):
+    for C in count(644):
         for i in range(4):
             if len(list(pyprimes.factorise(C+i))) != 4:
                 break
